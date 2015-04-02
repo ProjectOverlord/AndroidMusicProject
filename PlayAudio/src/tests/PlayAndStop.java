e7e5d7ee3fd955a41c70ee1c3a7123204cf80c77
Saeval <Saeval@users.noreply.github.com>
2015-04-02 11:27:46 +0200

    Fix for Ubuntu playing

--- PlayAudio/src/tests/PlayAndStop.java
+++ PlayAudio/src/tests/PlayAndStop.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
@@ -82,7 +84,10 @@ public class PlayAndStop {
 		try {
 			AudioInputStream audio = AudioSystem.getAudioInputStream(url);
 			
-			Clip clip = AudioSystem.getClip();
+			AudioFormat format = audio.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			
+			Clip clip = (Clip)AudioSystem.getLine(info);
 			clip.open(audio);
 				
 			clip.loop(1);
