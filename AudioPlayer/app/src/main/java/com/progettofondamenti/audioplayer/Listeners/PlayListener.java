ffa035aed0f774a986e1f4de29a56741be8e3b2c
abovethecloud <claudio.peroni93@gmail.com>
2015-04-24 23:16:43 +0200

    Experimented with buttons and listeners.
    Miserably failed, part 2.

---
+++ AudioPlayer/app/src/main/java/com/progettofondamenti/audioplayer/Listeners/PlayListener.java
@@ -0,0 +1,37 @@
+package com.progettofondamenti.audioplayer.listeners;
+
+import android.view.View;
+import android.widget.SeekBar;
+
+import com.progettofondamenti.audioplayer.BarUpdater;
+import com.progettofondamenti.audioplayer.iPlayer;
+
+import android.os.Handler;
+
+/**
+ * Terrible listener
+ */
+public class PlayListener implements View.OnClickListener {
+
+	iPlayer player;
+	SeekBar seekBar;
+	Handler handler;
+	BarUpdater barUpdater;
+
+
+	public PlayListener(iPlayer player, SeekBar seekBar, Handler handler, BarUpdater barUpdater) {
+		super();
+		this.player = player;
+		this.handler = handler;
+		this.barUpdater = barUpdater;
+		this.seekBar = seekBar;
+	}
+
+	@Override
+	public void onClick(View v) {
+		player.play();
+		seekBar.setMax(100);
+		seekBar.setProgress(player.getPlayerPosition());
+		handler.postDelayed(barUpdater, 100);
+	}
+}
