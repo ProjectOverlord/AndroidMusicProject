7c9809492c6d72fcec476649b5c372e0799d1a99
abovethecloud <claudio.peroni93@gmail.com>
2015-04-24 23:22:29 +0200

    Moving PlayListener because I'm having problems with git

--- AudioPlayer/app/src/main/java/com/progettofondamenti/audioplayer/buttons/PlayButton.java
+++ AudioPlayer/app/src/main/java/com/progettofondamenti/audioplayer/buttons/PlayButton.java
@@ -11,6 +11,7 @@ import android.widget.SeekBar;
 import com.progettofondamenti.audioplayer.BarUpdater;
 import com.progettofondamenti.audioplayer.R;
 import com.progettofondamenti.audioplayer.iPlayer;
+import com.progettofondamenti.audioplayer.listeners.PlayListener;
 
 /**
  * This class should be the one representing the button that calls the play()
