933d483d3205327c23c658bfe8d2547e1509b1c9
FraAndro <FraAndro@users.noreply.github.com>
2015-05-14 18:21:27 +0200

    Improved SettingsActivity class: now the user can select a background color for the app and pressing the arrow the changed are immediately applied

--- AudioPlayer/app/src/main/java/com/progettofondamenti/audioplayer/SettingsActivity.java
+++ AudioPlayer/app/src/main/java/com/progettofondamenti/audioplayer/SettingsActivity.java
@@ -26,7 +26,7 @@ import java.util.List;
 /**
  * Specific settings activity created to be able to select preferences
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +35,34 @@ public class SettingsActivity extends PreferenceActivity {
         // this method is deprecated, but it's necessary to provide support on older devices
         addPreferencesFromResource(R.xml.apppreferences);
 
+
     }
+
 
     @Override
     protected void onResume() {
         super.onResume();
         // Set background color of the activity
-        SharedPreferences sp=PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         MainActivity.setBGColor(sp.getString("pref_color", ""));
+
+        // Register OnSharedPreferenceChangeListener
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        // Unregister OnSharedPreferenceChangeListener
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_color")) {
+            // Set background color of the activity
+            MainActivity.setBGColor(sharedPreferences.getString(key, ""));
+        }
     }
 }
