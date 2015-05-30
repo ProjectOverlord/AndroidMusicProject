cfccf03410c06541dd16705ffbcee0685c3f8502
FraAndro <FraAndro@users.noreply.github.com>
2015-05-30 12:21:55 +0200

    Equalizer: few adjustments inside MusicService

--- EqualizerAudioPlayer/app/src/main/java/com/progettofondamenti/equalizeraudioplayer/MusicService.java
+++ EqualizerAudioPlayer/app/src/main/java/com/progettofondamenti/equalizeraudioplayer/MusicService.java
@@ -1,30 +1,20 @@
 package com.progettofondamenti.equalizeraudioplayer;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.ContentUris;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
-import android.os.PowerManager;
 import android.util.Log;
 
-/*
- * This is demo code to accompany the Mobiletuts+ series:
- * Android SDK: Creating a Music Player
- * 
- * Sue Smith - February 2014
+/**
+ * The aim of this class is to operate as a service for a mediaplayer
+ * that we want to pass to the EqualizerActivity
  */
-
 public class MusicService extends Service implements 
 MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
 MediaPlayer.OnCompletionListener {
